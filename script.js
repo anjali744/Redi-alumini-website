55f0e95005c178d2eb3e52426b5014d8843eb8be
Yui Suzuki <s.yui1122@gmail.com>
2023-11-22 18:42:19 +0100

    added css card section

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
+
 function filterFunction() {
   var input, filter, ul, li, a, i;
   input = document.getElementById("myInput");
